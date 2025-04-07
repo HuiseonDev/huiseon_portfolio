34834a4bbb7c166b2f2392d57e6910049a1dfc76
전희선 <designh2sun@gmail.com>
2025-04-07 23:44:45 +0900

    feat: 배경 텍스트 추가 및 3D 모델 뷰어에 줌 비활성화

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import ThreeTorus from "@components/ThreeTorus";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import GlassModel from "@components/GlassModel";
+import { Suspense } from "react";
 
 function App() {
   return (
-    <>
-      <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
-        <ThreeTorus />
-      </div>
-    </>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      <h1
+        style={{
+          fontSize: "1.5rem",
+          color: "lightGray",
+          pointerEvents: "none",
+          fontWeight: "500",
+          zIndex: 0,
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+        background text background text background text
+        <br />
+      </h1>
+      <Canvas
+        camera={{ position: [0, 0, 5], fov: 50 }}
+        style={{
+          width: "100vw",
+          height: "100vh",
+          zIndex: 10,
+        }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight intensity={2.6} />
+          <pointLight position={[10, 10, 10]} intensity={1} />
+          <directionalLight position={[5, 5, 5]} />
+          <GlassModel />
+
+          <OrbitControls enableZoom={false} />
+        </Suspense>
+      </Canvas>
+    </div>
   );
 }
 
