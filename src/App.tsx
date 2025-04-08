6690c15e9eb7219c46a3745fe21e948bdd388cf1
전희선 <designh2sun@gmail.com>
2025-04-08 15:51:34 +0900

    Style: 공통 스타일 임시 적용 및 gltf 파일 적용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,81 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import GlassModel from "@components/GlassModel";
 import { Suspense } from "react";
+import LemonModel from "@components/LemonModel";
+import MarbleModel from "@components/MarbleModel";
+import fontFamilies from "@styles/FontFamilies";
+import GlobalStyles from "@styles/GlobalStyles";
+import { Global } from "@emotion/react";
 
 function App() {
   return (
-    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
-      <h1
-        style={{
-          fontSize: "1.5rem",
-          color: "lightGray",
-          pointerEvents: "none",
-          fontWeight: "500",
-          zIndex: 0,
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-        background text background text background text
-        <br />
-      </h1>
-      <Canvas
-        camera={{ position: [0, 0, 5], fov: 50 }}
-        style={{
-          width: "100vw",
-          height: "100vh",
-          zIndex: 10,
-        }}
-      >
-        <Suspense fallback={null}>
-          <ambientLight intensity={2.6} />
-          <pointLight position={[10, 10, 10]} intensity={1} />
-          <directionalLight position={[5, 5, 5]} />
-          <GlassModel />
+    <>
+      <Global styles={GlobalStyles} />
+      <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+        <h1
+          style={{
+            fontFamily: fontFamilies.playfair,
+            fontStyle: "Italic",
+            fontWeight: 400,
+            fontSize: "2rem",
+            color: "lightGray",
+            pointerEvents: "none",
 
-          <OrbitControls enableZoom={false} />
-        </Suspense>
-      </Canvas>
-    </div>
+            zIndex: 0,
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+          background text background text background text
+          <br />
+        </h1>
+        <Canvas
+          camera={{ position: [0, 0, 5], fov: 50 }}
+          style={{
+            width: "100vw",
+            height: "100vh",
+            zIndex: 10,
+          }}
+        >
+          <Suspense fallback={null}>
+            <ambientLight intensity={2.6} />
+            <pointLight position={[10, 10, 10]} intensity={1} />
+            <directionalLight position={[5, 5, 5]} />
+            <LemonModel />
+            <MarbleModel />
+            <OrbitControls enableZoom={false} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </>
   );
 }
 
