6b9edb765349125547d098098b3bd82e4dae12bd
전희선 <designh2sun@gmail.com>
2025-06-11 15:30:42 +0900

    Feat: App 컴포넌트에 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { Global } from "@emotion/react";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Global styles={GlobalStyles} />
+      <RouterProvider router={router} />;
+    </>
+  );
 };
 
 export default App;
