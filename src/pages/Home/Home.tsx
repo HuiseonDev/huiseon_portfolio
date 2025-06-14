83e8edb3e76c9c3c6fc62a88b553fb3b1bbb22ab
전희선 <designh2sun@gmail.com>
2025-06-14 15:56:42 +0900

    Cont: videoClip 컴포넌트, profile 컴포넌트 적용

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,34 @@
+/** @jsxImportSource @emotion/react */
+import Profile from "@/components/Profile/Profile";
+import VideoClip from "@/components/videoClip/VideoClip";
+import { css } from "@emotion/react";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div css={test}>
+      <p css={subTitleStyle}>●&nbsp;&nbsp;&nbsp;안녕하세요! 👋</p>
+      <VideoClip />
+      <Profile />
+    </div>
+  );
 };
 
 export default Home;
+
+const test = css`
+  height: 1000px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin-top: 4rem;
+`;
+
+const subTitleStyle = css`
+  position: absolute;
+  left: 8%;
+  top: 0;
+  width: 30rem;
+  height: 4rem;
+  padding-left: 3rem;
+`;
