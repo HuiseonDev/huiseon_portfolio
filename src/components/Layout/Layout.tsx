be659f9111f06ca317b017b737d008c0764b388b
전희선 <designh2sun@gmail.com>
2025-06-19 15:20:34 +0900

    Feat: Layout에 Intersection Observer 기반 스크롤 감시 대상 로직 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import useScrollObserver from "@/hooks/useScrollObserver";
+import { useRef } from "react";
 
 const Layout = () => {
+  const sentinelRef = useRef<HTMLDivElement>(null);
+  const isScrolled = useScrollObserver(sentinelRef);
   return (
     <div>
-      <Header />
+      <Header isScrolled={isScrolled} />
+      <div ref={sentinelRef} style={{ height: "1px" }} />
       <Outlet />
       <Footer />
     </div>
