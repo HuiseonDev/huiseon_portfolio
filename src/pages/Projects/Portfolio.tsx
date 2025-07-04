4b2cf76cc30001833b46dcf3a4801f3849ba4988
전희선 <designh2sun@gmail.com>
2025-07-04 15:55:37 +0900

    Style: 프로젝트 서브 페이지 개발 진행중 문구 추가

--- src/pages/Projects/Portfolio.tsx
+++ src/pages/Projects/Portfolio.tsx
@@ -1,11 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-const Toucheese = () => {
+const Portfolio = () => {
   return <div css={developStyle}>개발 진행중입니다!</div>;
 };
 
-export default Toucheese;
+export default Portfolio;
 
 const developStyle = css`
   height: 100vh;
