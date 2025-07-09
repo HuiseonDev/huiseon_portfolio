ef4bd803c171f2a999a2fb947fe3d5005a520116
전희선 <designh2sun@gmail.com>
2025-07-09 17:46:57 +0900

    Feat: 페이지 이동 시 스크롤 위치 관리 기능 추가

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -1,11 +1,38 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { useLocation, useNavigationType } from "react-router-dom";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
+  const navigationType = useNavigationType();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
+  useLayoutEffect(() => {
+    // 뒤로가기
+    if (navigationType === "POP") {
+      const savedScrollPosition = sessionStorage.getItem(`scroll-${pathname}`);
+      if (savedScrollPosition) {
+        requestAnimationFrame(() => {
+          window.scrollTo(0, parseInt(savedScrollPosition));
+        });
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, navigationType]);
+
+  useLayoutEffect(() => {
+    // 스크롤 위치 저장
+    const handleScroll = () => {
+      sessionStorage.setItem(
+        `scroll-${pathname}`,
+        window.pageYOffset.toString()
+      );
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, [pathname]);
 
   return null;
