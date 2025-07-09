14229393b3066f0b7fddeda5deba026b4034690f
전희선 <designh2sun@gmail.com>
2025-07-09 17:45:21 +0900

    Refactor: 프로필 정보를 데이터 타입으로 분리하여 적용

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,6 +5,7 @@ import variables from "@/styles/Variables";
 import { TypoBodyMdB } from "@/styles/Common";
 import { breakPoints, mqMax } from "@/styles/BreakPoint";
 import { useMediaQuery } from "react-responsive";
+import { profile } from "@/data/profile";
 
 const Profile = () => {
   const isMo = useMediaQuery({ maxWidth: breakPoints.moMax });
@@ -24,24 +25,13 @@ const Profile = () => {
       <div css={contentsWrapper}>
         <section css={leftContentWrapper}>
           <span>
-            디자인 전공의 통찰력으로 문제를 정의하고
+            {profile.subTitleTop}
             <br />
-            개발로 실행 가능한 해답을 만듭니다.
+            {profile.subTitleBottom}
           </span>
-          <h2>
-            두 영역을 유기적으로 연결하는, 프론트엔드 개발자 전희선입니다.
-          </h2>
+          <h2>{profile.title}</h2>
         </section>
-        <span css={rightContentWrapper}>
-          UI/UX 에 대한 깊은 이해를 바탕으로 사용자 중심의 사고를 개발에
-          녹여내면서, 실제 사용성 테스트를 통해 프로젝트를 계속해서
-          개선해왔습니다. 이처럼 디자인과 개발을 동시에 이해할 수 있다는 점이
-          저만의 강점이라고 생각하며 사용자 니즈를 반영한 인터랙티브한 웹 경험을
-          만드는 데 흥미를 가지고 꾸준히 성장하고 있습니다. 또한 컴포넌트
-          설계부터 유지보수까지 신경 쓰며 반응형 웹과 웹 접근성을 고려한 UI
-          개발에도 집중하고 있습니다. 다양한 환경에서 누구나 편리하게 사용할 수
-          있는 웹 서비스를 만드는 것이 제 목표입니다.
-        </span>
+        <span css={rightContentWrapper}>{profile.description}</span>
       </div>
       <Button
         buttonText={"github"}
