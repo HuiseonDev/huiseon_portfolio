723e3b96e57da743da477a1a4fe3783b0086a25b
전희선 <designh2sun@gmail.com>
2025-07-29 13:08:34 +0900

    Feat: Lottie를 활용한 로딩 컴포넌트 적용

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,55 @@
 /** @jsxImportSource @emotion/react */
 
-const Loading = () => {
-  return <div>로딩</div>;
+import animationData from "@/assets/json/loading.json";
+import { TypoCapSmR } from "@/styles/Common";
+import variables from "@/styles/Variables";
+import { css } from "@emotion/react";
+import Lottie from "lottie-react";
+
+interface ILoading {
+  size?: "small" | "big";
+  phrase?: string;
+}
+
+const Loading = ({ size = "small", phrase }: ILoading) => {
+  return (
+    <div css={lottieBox(size)}>
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        autoplay={true}
+        rendererSettings={{ preserveAspectRatio: "xMidYMid slice" }}
+        style={{ width: size === "big" ? "20rem" : "14rem" }}
+      />
+      {phrase && <p className="loadingText">{phrase}</p>}
+    </div>
+  );
 };
 
 export default Loading;
+
+const Montserrat = css`
+  font-family: "Montserrat", sans-serif;
+  font-size: ${TypoCapSmR};
+  color: ${variables.colors.gray700};
+`;
+
+const lottieBox = (size: string) => css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  ${Montserrat}
+
+  ${size === "small" && "min-height: 50vh;"}
+  ${size === "big" &&
+  `    
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);`}
+
+  .loadingText {
+    font-weight: 500;
+  }
+`;
