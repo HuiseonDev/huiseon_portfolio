8a07584883ab60a6120afb4f978b9b3996796651
전희선 <designh2sun@gmail.com>
2025-07-29 13:02:39 +0900

    Feat: 앱 로딩 상태 처리 및 에러 바운더리 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import { Global } from "@emotion/react";
+import { Global, ThemeProvider } from "@emotion/react";
 import GlobalStyles from "./styles/GlobalStyles";
+import variables from "./styles/Variables";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense, useDeferredValue, useEffect, useState } from "react";
+import Loading from "./components/Loading/Loading";
+import Error from "./components/Error/Error";
 
 const App = () => {
+  const [isReady, setIsReady] = useState(false);
+  const deferredReady = useDeferredValue(isReady);
+
+  const skipLoading = sessionStorage.getItem("skipLoading");
+
+  useEffect(() => {
+    if (!skipLoading) {
+      const timer = setTimeout(() => setIsReady(true), 1300);
+      return () => clearTimeout(timer);
+    } else {
+      setIsReady(true);
+      sessionStorage.removeItem("skipLoading");
+    }
+  }, [skipLoading]);
+
   return (
-    <>
+    <ThemeProvider theme={variables}>
       <Global styles={GlobalStyles} />
-      <RouterProvider router={router} />
-    </>
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<Loading />}>
+          {!deferredReady ? (
+            <Loading size="big" phrase="Please wait..." />
+          ) : (
+            <RouterProvider router={router} />
+          )}
+        </Suspense>
+      </ErrorBoundary>
+    </ThemeProvider>
   );
 };
 
